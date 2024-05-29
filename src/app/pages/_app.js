e15509c9608c1f10c9415759041220ea55ccec3d
Sélèna Boullée <sboullee@kalya-sante.com>
2024-05-29 14:42:40 +0200

    feat: create project travel-book

---
+++ src/app/pages/_app.js
@@ -0,0 +1,8 @@
+import 'leaflet/dist/leaflet.css';
+import '../styles/globals.css'; // Assurez-vous d'avoir un fichier CSS global
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
